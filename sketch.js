474612f27cac1e750e9ee0897a3a88c24503f2e2
Symartinez21 <sophia.martinez@sjsu.edu>
2021-10-07 13:55:26 -0700

    2b.1 randomizer but ??

---
+++ sketch.js
@@ -0,0 +1,14 @@
+//where do I want to go get boba
+let boba = ["7leaves", "bobaholics", "happyLemon", "tenRen", "85c"]
+
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+
+console.log(boba[2]);
+
+}
+
+function draw();
+
+}
